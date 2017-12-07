77564761893999ac9371c428daa02a3cebfa870c
Troy Reyles <troyreyles@gmail.com>
2017-12-06 22:16:00 -0800

    started discover card

--- src/components/DiscoverCard.js
+++ src/components/DiscoverCard.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import truncate from 'lodash/truncate';
 
-const DiscoverCard = ({ media }) => (
-  <div className="discover-card">
-    <h2>{media.title}</h2>
-    <h2>{media.release_date}</h2>
-  </div>
-);
+export default function DiscoverCard({ media }) {
+  return (
+    <div className="discover-card">
+      <div className="poster-info-container">
+        <div className="discover-poster">
+          <div className="img-container">
+            <img
+              src={`https://image.tmdb.org/t/p/w154${media.poster_path}`}
+              alt={`${media.title ? media.title : media.name} poster`}
+            />
+            <span className="rating-tag">
+              <h3>{media.vote_average.toFixed(1)}</h3>
+            </span>
+          </div>
+        </div>
+        <div className="discover-info">
+          <h2>
+            {truncate(media.title ? media.title : media.name, {
+              length: 40,
+              separator: ' '
+            })}
+          </h2>
+          <h3>Adventure, Drama, Family</h3>
+          <h3>
+            {typeof media.release_date === 'string'
+              ? media.release_date.substring(0, 4)
+              : media.first_air_date.substring(0, 4)}
+          </h3>
+          <p>
+            {truncate(media.overview, { length: 110, separator: ' ' })}
+          </p>
+        </div>
+      </div>
+      <div className="discover-footer">
+        footer
+      </div>
+    </div>
+  );
+}
 
-export default DiscoverCard;
+DiscoverCard.propTypes = {
+  media: PropTypes.shape({
+    title: PropTypes.string,
+    release_date: PropTypes.string
+  }).isRequired
+};
