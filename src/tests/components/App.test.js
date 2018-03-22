e00d33def400e2cd76a4f0bccdc2888a37a3e8fc
Troy Reyles <troyreyles@gmail.com>
2018-03-21 17:38:32 -0700

    webpack configured for production

--- src/tests/components/App.test.js
+++ src/tests/components/App.test.js
@@ -5,73 +5,71 @@ import App from '../../components/App';
 import Home from '../../components/Home';
 import NotFound from '../../components/NotFound';
 
-
 describe('App', () => {
   let component;
 
   beforeEach(() => {
-      component = shallow(<App />);
-  })
+    component = shallow(<App />);
+  });
 
   it('should render correctly', () => {
-    expect(component).toMatchSnapshot()
-  })
+    expect(component).toMatchSnapshot();
+  });
 
   it('should render render `Home` if currentUser is null', () => {
     const mountedComponent = mount(
-      <MemoryRouter initialEntries={[ '/' ]}>
-        <App/>
+      <MemoryRouter initialEntries={['/']}>
+        <App />
       </MemoryRouter>
-    )
+    );
 
-    expect(mountedComponent.find(Home)).toHaveLength(1)
-  })
+    expect(mountedComponent.find(Home)).toHaveLength(1);
+  });
 
   it('should render `NotFound` if visiting unknown path', () => {
     const mountedComponent = mount(
-      <MemoryRouter initialEntries={[ '/unknownpath' ]}>
-        <App/>
+      <MemoryRouter initialEntries={['/unknownpath']}>
+        <App />
       </MemoryRouter>
-    )
-    
-    expect(mountedComponent.find(NotFound)).toHaveLength(1)
-  })
+    );
+
+    expect(mountedComponent.find(NotFound)).toHaveLength(1);
+  });
 
   it('should push new media with `addToLibrary`', () => {
-    const media = { one: 'one' }
+    const media = { one: 'one' };
 
-    component.instance().addToLibrary(media)
-    expect(component.state().library).toHaveLength(1)
-  })
+    component.instance().addToLibrary(media);
+    expect(component.state().library).toHaveLength(1);
+  });
 
   it('should push new media to top (index 0) with `addToLibrary`', () => {
     component.setState({
-      library: [{ one: 'one'}]
-    })
+      library: [{ one: 'one' }]
+    });
 
-    const media = { two: 'two' }
+    const media = { two: 'two' };
 
-    component.instance().addToLibrary(media)
-    expect(component.state().library[0]).toEqual(media)
-  })
+    component.instance().addToLibrary(media);
+    expect(component.state().library[0]).toEqual(media);
+  });
 
-  it('should remove new media with `removeFromLibrary`', () => {
-    const media = { one: 'one' }
-    const mediaTwo = {two: 'two'}
+  it('should remove media with `removeFromLibrary`', () => {
+    const media = { one: 'one' };
+    const mediaTwo = { two: 'two' };
 
     component.setState({
       library: [media, mediaTwo]
-    })
+    });
 
-    component.instance().removeFromLibrary(media)
+    component.instance().removeFromLibrary(media);
 
-    expect(component.state().library).toHaveLength(1)
-    expect(component.state().library[0]).toEqual(mediaTwo)
-  })
+    expect(component.state().library).toHaveLength(1);
+    expect(component.state().library[0]).toEqual(mediaTwo);
+  });
 
   it('should load recommended media on user request', () => {
-    component.instance().addRecommended()
-    expect(component.state().library).toHaveLength(14)
-
-  })
-})
+    component.instance().addRecommended();
+    expect(component.state().library).toHaveLength(14);
+  });
+});
